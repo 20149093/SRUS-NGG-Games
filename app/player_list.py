68348eb4115c6357d323324cc722d0c555aca4a5
Nelson Gomez <20149093@tafe.wa.edu.au>
2026-03-23 00:13:59 +0800

    feature: add is_empty and insert_head methods to PlayerList

--- app/player_list.py
+++ app/player_list.py
@@ -1,5 +1,25 @@
 # Step 3 PlayerList with a private instance variable = None
+from player_node import PlayerNode
+
 
 class PlyerList:
     def __init__(self):
         self.__head = None
+# Create a method that allows me to insert a new node at the head (it's empty or not) return a boolean
+    def is_empty(self) -> bool:
+        return self.__head is None
+
+    def insert_head(self, player):
+        # Create a vehicle for our player
+        new_node = PlayerNode(player)
+        # it is empty then
+        if self.__head.is_empty():
+            self.__head = new_node
+        # I got someone in the list
+        else:
+            # the new vehicle is pointing to the one who was before
+            new_node.next = self.__head
+            # the old one has someone else before him
+            self.__head.prev = new_node
+            # the list head is now the new node
+            self.__head = new_node
--- app/player_node.py
+++ app/player_node.py
@@ -1,23 +1,16 @@
 # Step 4 create a class PlayerNode passing player then 3 private instances two of them = None
-# Set getters and setters as necessary then create a property call key which returns uid for the player object
+# Set getters and setters as necessary as then create a property call key which returns uid for the player object
 
 class PlayerNode:
     def __init__(self, player):
         self.__player = player
-        self.__previous_player = None
         self.__next_player = None
+        self.__previous_player = None
+
 
     @property
     def player(self):
         return self.__player
-
-    @property
-    def previous_player(self):
-        return self.__previous_player
-
-    @previous_player.setter
-    def previous_player(self, node):
-        self.__previous_player = node
 
     @property
     def next_player(self):
@@ -26,6 +19,14 @@ class PlayerNode:
     @next_player.setter
     def next_player(self, node):
         self.__next_player = node
+
+    @property
+    def previous_player(self):
+        return self.__previous_player
+
+    @previous_player.setter
+    def previous_player(self, node):
+        self.__previous_player = node
 
     @property
     def key(self):
