54d95647f1d507174e9f1385da3c253989319434
Nelson Gomez <20149093@tafe.wa.edu.au>
2026-03-23 01:34:31 +0800

    test: update player list tests to reflect correct behavior and attribute access

---
+++ test/player_list_test.py
@@ -0,0 +1,21 @@
+import unittest
+from app.player import Player
+from app.player_list import PlayerList
+
+class PlayerListTest(unittest.TestCase):
+    def test_insert_head_empty_list(self):
+        # what I want to do here is to test inserting a player into an empty list
+        plist = PlayerList()
+        p1 = Player("1", "NGG")
+        plist.insert_head(p1)
+        self.assertIsNotNone(plist._PlayerList__tail)
+
+    def test_insert_not_empty_list(self):
+        plist = PlayerList()
+        plist.insert_head(Player("1", "First"))
+        plist.insert_head(Player("2", "Second"))
+        # Now the head should be the second inserted player
+        self.assertEqual(plist._PlayerList__head.player.name, "Second")
+
+if __name__ == '__main__':
+    unittest.main()
