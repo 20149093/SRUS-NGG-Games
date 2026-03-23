43f38d000d0ab71936ccb702d709afcf5b307aa4
Nelson Gomez <20149093@tafe.wa.edu.au>
2026-03-23 09:52:22 +0800

    fix: align PlayerList behavior with unit tests and fix attribute access issues

--- app/player_list.py
+++ app/player_list.py
@@ -21,9 +21,9 @@ class PlayerList:
         # I got someone in the list
         else:
             # the new vehicle is pointing to the one who was before
-            new_node.next = self.__head
+            new_node.next_player= self.__head
             # the old one has someone else before him
-            self.__head.prev = new_node
+            self.__head.previous_player= new_node
             # the list head is now the new node
             self.__head = new_node
 
@@ -42,3 +42,33 @@ class PlayerList:
             new_node.previous_player = self.__tail
             self.__tail.next_player = new_node
             self.__tail = new_node
+
+    # Step 7 d. add two methods - delete an item from head and one from the tail
+
+    def delete_head(self):
+        # It's removing the fist node from the list
+        if self.is_empty():
+            return None
+        removed_node = self.__head
+        if self.__head == self.__tail:
+            self.__head = None
+            self.__tail = None
+        else:
+            self.__head = self.__head.next_player
+            self.__head.previous_player = None
+        return removed_node.player
+
+    def delete_tail(self):
+        if self.is_empty():
+            return None
+
+        removed_node = self.__tail
+
+        if self.__head == self.__tail:
+            self.__head = None
+            self.__tail = None
+        else:
+            self.__tail = self.__tail.previous_player
+            self.__tail.next_player = None
+
+        return removed_node.player
--- test/player_list_test.py
+++ test/player_list_test.py
@@ -22,6 +22,29 @@ class PlayerListTest(unittest.TestCase):
         plist.insert_tail(Player("99", "Last Player"))
         self.assertEqual(plist._PlayerList__tail.player.name, "Last Player")
 
+    def test_delete_head(self):
+        plist = PlayerList()
+        p1 = Player("1", "First")
+        p2 = Player("2", "Second")
+        plist.insert_tail(p1)
+        plist.insert_head(p2)
+
+        removed_player = plist.delete_head()
+
+        self.assertEqual(removed_player.uid, "2")
+        self.assertEqual(plist._PlayerList__head.player.uid, "1")
+
+    def test_delete_tail(self):
+        plist = PlayerList()
+        plist.insert_tail(Player("1", "First"))
+        plist.insert_tail(Player("2", "Last"))
+
+        removed = plist.delete_tail()
+        self.assertEqual(removed.uid, "2")
+        self.assertEqual(plist._PlayerList__tail.player.uid, "1")
+        self.assertIsNone(plist._PlayerList__tail.next_player)
+
 
 if __name__ == '__main__':
     unittest.main()
+
