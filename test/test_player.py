78c248dba98b18337c1deff1c5218dbc03e2e8ed
Nelson Gomez <20149093@tafe.wa.edu.au>
2026-05-18 12:47:36 +0800

    test: add player sort and comparison tests

--- test/test_player.py
+++ test/test_player.py
@@ -8,6 +8,28 @@ class TestPlayer(unittest.TestCase):
         self.assertEqual(player.uid, "101")
         self.assertEqual(player.name, "Nelson")
 
+    def test_players_can_be_compared_by_score(self):
+        alice = Player("01", "Alice", score=10)
+        bob = Player("02", "Bob", score=5)
+
+        self.assertTrue(bob < alice)
+
+    def test_sort_players(self):
+        players = [
+            Player("01", "Alice", score=10),
+            Player("02", "Bob", score=5),
+            Player("03", "Charlie", score=15),
+        ]
+
+        sorted_players = sorted(players)
+        manually_sorted_players = [
+            Player("02", "Bob", score=5),
+            Player("01", "Alice", score=10),
+            Player("03", "Charlie", score=15),
+        ]
+
+        self.assertListEqual(sorted_players, manually_sorted_players)
+
 
 if __name__ == "__main__":
     unittest.main()
